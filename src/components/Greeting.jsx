0f2dbecdd000b0f0439868553635f104836f82a2
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2025-02-07 16:38:42 -0500

    greeting mobile fix

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -5,7 +5,7 @@ function Greeting() {
     const user = useSelector((state) => state.user);
     const navigate = useNavigate();
 
-    return (<div style={{border:"1px solid gold", padding:".5em", borderRadius:"15px", display:"flex", flexDirection:"column", gap:".5em"}}>
+    return (<div style={{border:"1px solid gold", padding:".5em", borderRadius:"15px", display:"flex", flexDirection:"column", gap:".5em", flexWrap:"wrap", maxWidth:"80%"}}>
     {user.user.isAdmin && !user.user.isBusiness && <>
             <h4>Welcome Back! </h4>
             <p>Your Admin Permissions allows you to like cards and view the Sandbox/CRM Page.</p>
@@ -19,10 +19,10 @@ function Greeting() {
     {user.user.isBusiness && user.user.isAdmin && <>
         <h4>Welcome Back !</h4>
         <p>Your Admin & Business Permissions allows you to view Sandbox/CRM Page, Create/Edit/Delete/Like Business Cards.</p>
-        <div className="greeting-btns" style={{display:"flex", justifyContent:"center", alignItems:"center", gap:".3em"}}>
+            <div className="greeting-btns" style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: ".3em", flexWrap: "wrap" }}>
                 <NavLink to="/liked-cards"> <button>Liked Cards</button> </NavLink>
                 <NavLink to="/business/user"> <button>My Cards</button> </NavLink>
-                <NavLink to="/business/add-business"> <button>Add Business Card</button> </NavLink>
+                <NavLink to="/business/add-business"> <button>Add Business</button> </NavLink>
                 <NavLink to="/sandbox-crm"> <button>Sandbox CRM</button> </NavLink>
         </div>
     </>}
@@ -30,24 +30,24 @@ function Greeting() {
     {user.user.isBusiness && !user.user.isAdmin && <>
         <h4>Welcome Back !</h4>
         <p>Your Business Permissions allows you to, Create/Edit/Delete/Like Business Cards.</p>
-            <div className="greeting-btns" style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: ".3em" }}>
+            <div className="greeting-btns" style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: ".3em", flexWrap: "wrap" }}>
                 <NavLink to="/liked-cards"> <button>Liked Cards</button> </NavLink>
                 <NavLink to="/business/user"> <button>My Cards</button> </NavLink>
-                <NavLink to="/business/add-business"> <button>Add Business Card</button> </NavLink>
+                <NavLink to="/business/add-business"> <button>Add Business</button> </NavLink>
             </div>
     </>}
 
     {user.user._id != "" && !user.user.isBusiness && !user.user.isAdmin && <>
         <h4>Welcome Back!</h4>
             <p>Your Regular User Permissions allows you to Like and Save Business Cards to Your Liked Cards Page.</p>
-            <div className="greeting-btns" style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: ".3em" }}>
+            <div className="greeting-btns" style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: ".3em", flexWrap: "wrap" }}>
                 <NavLink to="/liked-cards"> <button>Liked Cards</button> </NavLink>
             </div>
     </>}
 
     {user.user._id == "" && <>
             <h4>Login or Register to Like or Add your Business Cards to our Directory.</h4>
-            <div className="greeting-btns" style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: ".3em" }}>
+            <div className="greeting-btns" style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: ".3em", flexWrap: "wrap" }}>
                 <NavLink to="/login"> <button>Login</button> </NavLink>
                 <NavLink to="/register"> <button>Register</button> </NavLink>
             </div>
